085fcd2b18833598988464c8764baa69a51d22f4
rafatreggia <rafaeltreggia24@gmail.com>
2025-04-12 10:03:19 -0300

    primer subida a repositorio

---
+++ frontend/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import React, { useState } from "react";
+export default function Home() {
+  return (
+    <div>
+      <h1>page</h1>
+      <Button>Push</Button>
+    </div>
+  );
+}
